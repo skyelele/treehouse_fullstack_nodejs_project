11adf3042683445e99960982f15a815369c8c201
Skye Leahy <skyeleahy@protonmail.com>
2019-03-05 21:40:30 -0800

    add routes practice

---
+++ routes_practice.js
@@ -0,0 +1,32 @@
+/** 
+ * We've been tasked to continue with 
+ * this prototype site. Have a look in 
+ * the index.js and routes.js file and 
+ * familiarize yourself. In the routes.js,
+ *  just like the other two functions 
+ * create an about function for a /about 
+ * route. Have it print out "About\n" to 
+ * the response. Remember to make this 
+ * accessible by other files.
+ */
+
+function root(request, response) {
+    if(request.url == "/") {
+        response.writeHead(200, {'Content-type': "text"});
+        response.end("Home\n");
+    }
+}
+
+function contact(request, response) {
+    if(request.url == "/contact") {
+        response.writeHead(200, {'Content-type': "text/plain"});
+        response.end("Contact\n");
+    }
+}
+
+function about(request, response) {
+    if(request.url == "/about") {
+        response.writeHead(200, {'Content-type': "text/plain"});
+        response.end("Contact\n");
+    }
+}
