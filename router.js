29c36a17a4729e9c35b483a356f3f25577ce39d0
Skye Leahy <skyeleahy@protonmail.com>
2019-03-06 05:25:49 +0000

    add code to render Home + User routes, plus add code for populating user information

--- router.js
+++ router.js
@@ -1,4 +1,6 @@
-//2. Handle HTTP route GET /and POST / i.e. Home
+var Profile = require("./profile.js");
+
+//Handle HTTP route GET /and POST / i.e. Home
 function home(req, res) {
   if(req.url === "/") {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -7,21 +9,38 @@ function home(req, res) {
     res.end("Footer\n");
   }
 }
-  //if url ==="/" && GET
-    //show search
-  //if url =="/" && POST
-    //redirect to /:username
 
-//3. Handle HTTP route GET /:username i.e. /shalkers
+//Handle HTTP route GET /:username i.e. /shalkers
 function user(req, res) {
   var username = req.url.replace("/", "");
   if(username.length > 0) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write("Header\n");
-    res.write(username + "\n");
-    res.end("footer\n");
+   //get json from Treehouse
+    var studentProfile = new Profile(username);
+    //on "end"
+    studentProfile.on("end", function(profileJSON){
+      //show profile
+      
+      //store the values which we need
+      var values = {
+        avatarUrl: profileJSON.gravatar_url, 
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+      // simple response
+      // (we need to write an END to EVERY callback that we use)
+      res.write(values.username + " has " + values.badges + " badges\n");;
+      res.end("Footer\n");
+    });
+    studentProfile.on("error", function(error){
+    //show error
+      res.write(error.message + "\n");
+      res.end("Footer\n");
+    });
   }
-}
+};
 
 module.exports.home = home;
 module.exports.user = user;
